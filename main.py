312b063f5346b9d8661c9be407416b72d707cedc
baxianguohai <luomu628@qq.com>
2018-05-23 09:11:41 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+# -*- coding: UTF-8 -*-
+
+import json
+import random
+
+
+def change(input_data, length, key, mix, max):
+    for i in range(length):
+        input_data[i][key] = random.randint(mix, max)
+
+
+def change_detail(input_data, length):
+    for i in range(length):
+        str_tmp = input_data[i]["ansDetail"]
+        str_pre = str_tmp[:str_tmp.find("#")]
+        # print(str_pre)
+        str_sur = str_tmp[str_tmp.find("#") + 1:]
+        str_score = ""
+        for score in str_sur.split(","):
+            try:
+                if int(float(score)) < 85:
+                    int_score = float(score) + random.randint(85 - int(float(score)), 95 - int(float(score)))
+                    str_score += str(int_score) + ","
+                else:
+                    str_score += score + ","
+            except ValueError:
+                    pass
+        if not str_score.endswith(","):
+            str_score += ","
+        input_data[i]["ansDetail"] = str_pre + "#" + str_score
+
+
+data = input("请输入post提交的data：\n")
+data_json = json.loads(data.strip())
+length = len(data_json)
+change(data_json, length, "score", 86, 95)  # 自己设置分数区间
+change(data_json, length, "accuracy", 93, 98)
+change(data_json, length, "fluency", 94, 100)
+change(data_json, length, "complete", 96, 100)
+change_detail(data_json, length)
+print(json.dumps(data_json))
+
+
+
+
+
+
